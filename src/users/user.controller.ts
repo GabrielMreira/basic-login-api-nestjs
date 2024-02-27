d3514429a70a8b8cdd6e3975eedbd9b25e25ee51
Gabriel Moreira <107933147+GabrielMreira@users.noreply.github.com>
2024-02-26 21:55:13 -0300

    Construção do controler de usuarios

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, ParseUUIDPipe, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController{}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getAll() {}
+
+  @Post
+  create(@Body body) {}
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {}
+
+  @Patch(':id')
+  update(@Param('id', ParseUUIDPipe) id: string) {}
+
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: string) {}
+}
